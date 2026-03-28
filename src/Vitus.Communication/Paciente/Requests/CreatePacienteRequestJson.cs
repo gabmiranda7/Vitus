ec08fa027a97686865495bf62fb4372c039f8dfd
gabmiranda7 <gab.miranda404@gmail.com>
2026-03-28 19:22:08 -0300

    feat(communication): create dtos for Paciente creation flow

---
+++ src/Vitus.Communication/Paciente/Requests/CreatePacienteRequestJson.cs
@@ -0,0 +1,7 @@
+﻿namespace Vitus.Communication.Paciente.Requests
+{
+    public class CreatePacienteRequestJSON
+    {
+        public string Nome { get; set; }
+    }
+}
---
+++ src/Vitus.Communication/Paciente/Responses/PacienteResponseJson.cs
@@ -0,0 +1,8 @@
+﻿namespace Vitus.Communication.Paciente.Responses
+{
+    public class PacienteResponseJson
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
